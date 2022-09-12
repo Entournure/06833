89e93a864772110fa22c8f229e5c1012ef838d2a
Entournure <seun7763@naver.com>
2022-09-12 20:51:15 +0900

    connect fix

--- TestScript.py
+++ TestScript.py
@@ -1,5 +1,4 @@
 from appium import webdriver
-import pytest
 
 APPIUM_SERVER_ADDRESS = 'http://localhost:4723/wd/hub'
 automation_name = 'UiAutomator2'
@@ -10,17 +9,11 @@ app = 'D:\LINE.apk'
 appPackage = 'jp.naver.line.android'
 appActivity = 'activity.SplashActivity'
 
+# 아래 필요한 정보들을 기입해 줍니다.
+android_caps = {'automationName': automation_name, 'platformName': platform_name,
+                'platformVersion': platform_version, 'deviceName': device_name,
+                'app': app, 'appPackage': appPackage, 'appActivity': appActivity, 'noReset': True}
+driver = webdriver.Remote(APPIUM_SERVER_ADDRESS, android_caps)
 
-@pytest.fixture(scope="module")
-def driver():
-    # 아래 필요한 정보들을 기입해 줍니다.
-    android_caps = {'automationName': automation_name, 'platformName': platform_name,
-                    'platformVersion': platform_version, 'deviceName': device_name,
-                    'app': app, 'appPackage': appPackage, 'appActivity': appActivity, 'noReset': True}
-    driver = webdriver.Remote(APPIUM_SERVER_ADDRESS, android_caps)
-    yield driver
-    # 테스트 종료시 앱 종료
-    # driver.quit()
-
-def test_run(driver):
+def test_login():
     assert True
